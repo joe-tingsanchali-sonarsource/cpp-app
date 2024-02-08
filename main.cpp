c6507de51189b89eb8a89f8b54c36a14a0b5bb53
Cole Gannaway <156719330+cole-gannaway-sonarsource@users.noreply.github.com>
2024-02-08 15:33:05 -0600

    added security vulnerabilities for sonarqube analysis

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <cstring>
 #include "functions.h"
+#include <random>
+#include <string.h>
+#include <stdio.h>
+
+int generateRandomId() 
+{
+  int random_int = std::rand(); // Should use functions which rely on a strong random number generator, not a pseudo-random number generator becasuse it can be predicted
+}
+
+void badInputFunction(){
+    char str[20];
+    gets(str); // Noncompliant; `str` buffer size is not checked and it is vulnerable to overflows
+}
 
 int main()
 {
@@ -8,6 +21,16 @@ int main()
     double avg = getAverage(values, 10);
     char array[10];
     void *pos = memchr(array, '@', 42);
-    std::cout << avg;
+    std::cout << avg << std::endl;
+
+    // Random Id issue
+    int id = generateRandomId();
+    std::cout << "Random'ish id = " << id << std::endl;
+
+    // Hardcoded IP issue
+    char * host = "10.10.0.1"; // Sensitive
+    std::cout << "Hardcoded Server IP = " << id << std::endl;
+
+    return 0;
 }
 
